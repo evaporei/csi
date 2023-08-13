f09473feceac5eeb681ebf947590b8ac5ab9a24a
Eva Pace <eba.pachi@gmail.com>
2023-08-13 15:44:48 -0300

    intro-systems: sum to n, first test pass

--- intro-systems/x86-64-programming/README.md
+++ intro-systems/x86-64-programming/README.md
@@ -6,3 +6,5 @@ nasm -g -f elf64 hello.asm; and ld -o hello hello.o; and ./hello
 # bash/zsh
 nasm -g -f elf64 hello.asm && ld -o hello hello.o && ./hello
 ```
+
+# 2. Adding numbers 1 to n
--- intro-systems/x86-64-programming/sum_to_n.asm
+++ intro-systems/x86-64-programming/sum_to_n.asm
@@ -1,4 +1,17 @@
+; sample base code in C
+; int sum_to_n(n) {
+;     int total = 0;
+;
+;     for (int i = 0; i <= n; i++)
+;         total += i;
+;
+;     return total;
+; }
 section .text
 global sum_to_n
+
+; n = rdi
+; total will go into rax
 sum_to_n:
+	mov rax, 0
 	ret

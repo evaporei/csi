c32a5281fc9ebc99f34a03f9ae83a23c5832370c
Eva Pace <eba.pachi@gmail.com>
2023-08-20 18:32:11 -0300

    intro-systems: finish pangram using bitwise ops

--- intro-systems/x86-64-programming/pangram.asm
+++ intro-systems/x86-64-programming/pangram.asm
@@ -1,4 +1,40 @@
+; ; simple (lowercase + no err handling):
+; bool checkIfPangram(char * sentence){
+;     unsigned int letters = 0;
+;
+;     for (char *c = sentence; *c != '\0'; c++) {
+;         letters = letters | (1 << (*c - 97));
+;     }
+;
+;     return letters == 0x03ffffff;
+; }
+; ; complete:
+; bool pangram(char * sentence){
+;     unsigned int letters = 0;
+;
+;     for (char *c = sentence; *c != '\0'; c++) {
+;         letters |= (1 << ((*c | 32) - 97));
+;     }
+;
+;     return (letters & 0x03ffffff) == 0x03ffffff;
+; }
 section .text
 global pangram
 pangram:
+	mov eax, 0
+	mov edx, 0
+.loop:
+	; ecx = current char being accessed
+	movzx ecx, byte [rdi]
+	cmp ecx, 0
+	je .end
+	inc rdi
+	or ecx, 32 ; "to lowercase"
+	sub ecx, 97 ; 'a' -> will give 0-25 value
+	bts edx, ecx
+	jmp .loop
+.end:
+	and edx, 0x03ffffff
+	cmp edx, 0x03ffffff
+	sete al
 	ret

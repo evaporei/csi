4d252e88032fb7d3f8899432a7e4f932aa510d8f
Eva Pace <eba.pachi@gmail.com>
2023-08-21 09:00:33 -0300

    intro-systems: impl matrix asm

--- intro-systems/further-x86-64/matrix.asm
+++ intro-systems/further-x86-64/matrix.asm
@@ -6,4 +6,17 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+
+	; // Some C
+	; int rows = 3;
+	; int cols = 4;
+	; // Accessing elements using single index
+	; int rindex = 1;
+	; int cindex = 2;
+	; int index = rindex * cols + cindex;
+	; int value_flat = *(*matrix + index);
+
+	imul rcx, rdx ; rindex * cols
+	add rcx, r8 ; + cindex
+	mov rax, [rdi + rcx * 4] ; int = 4 bytes
 	ret

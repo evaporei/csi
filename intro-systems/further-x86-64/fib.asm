8fb7adef1f6e3f32c2f66cb96ea4ddbf79d4cff9
Eva Pace <eba.pachi@gmail.com>
2023-08-21 07:58:19 -0300

    intro-systems: fib impl asm

--- intro-systems/further-x86-64/fib.asm
+++ intro-systems/further-x86-64/fib.asm
@@ -1,4 +1,22 @@
 section .text
 global fib
 fib:
+   mov eax, edi
+   cmp edi, 1
+   jle .end
+
+   push rbx
+   mov ebx, edi
+   sub edi, 1
+   call fib
+
+   push r12
+   mov r12d, eax
+   lea edi, [rbx - 2]
+   call fib
+   add eax, r12d
+
+   pop r12
+   pop rbx
+.end:
    ret

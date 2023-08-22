36c1abfcb33ef262d76d509ccc3f8f7569e99f4b
Eva Pace <eba.pachi@gmail.com>
2023-08-22 09:46:19 -0300

    intro-systems: realign stack in fib exercise

--- intro-systems/further-x86-64/fib.asm
+++ intro-systems/further-x86-64/fib.asm
@@ -13,9 +13,14 @@ fib:
    push r12
    mov r12d, eax
    lea edi, [rbx - 2]
+
+   ; realign stack to 16 bytes
+   sub rsp, 8
+
    call fib
    add eax, r12d
 
+   add rsp, 8
    pop r12
    pop rbx
 .end:

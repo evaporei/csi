9ccf7286e9938ba66515e236d436cb291e3ad770
Eva Pace <eba.pachi@gmail.com>
2023-09-24 00:01:19 -0300

    comp-networks/reverse-proxy: first working nginy!!!

--- comp-networks/reverse-proxy/nginy.c
+++ comp-networks/reverse-proxy/nginy.c
@@ -1,12 +1,90 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#define PROJECT_NAME "nginy"
+#define PORT "8080"
+#define BACKLOG -1
+#define BUF_SIZE 1024
 
-int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
+int main(void) {
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    struct addrinfo hints, *res;
+    char buf[BUF_SIZE] = {0};
+    int status, sockfd, new_fd, bytes_read, bytes_sent;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // socket type = TCP
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+    if ((status = getaddrinfo(NULL, PORT, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+        perror("Socket creation failed");
         return 1;
     }
-    printf("This is project %s.\n", PROJECT_NAME);
+
+    if ((status = bind(sockfd, res->ai_addr, res->ai_addrlen)) != 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    freeaddrinfo(res);
+
+    if ((status = listen(sockfd, BACKLOG)) != 0) {
+        perror("listen failed");
+        return 1;
+    }
+
+    addr_size = sizeof(their_addr);
+    while (true) {
+        if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size)) < 0) {
+            perror("accept failed");
+            return 1;
+        }
+
+        while (true) {
+            bytes_read = recv(new_fd, buf, BUF_SIZE, 0);
+            // client closed connection
+            if (bytes_read == 0) {
+                // go to next client
+                goto close_client;
+            }
+
+            if (bytes_read < 0) {
+                perror("recv failed");
+                return 1;
+            }
+
+            // TODO: loop over sent data, it may have been split
+            if ((bytes_sent = send(new_fd, buf, bytes_read, 0)) < 0) {
+                perror("send failed");
+                return 1;
+            }
+
+            // seems safe to assume we've finished our work
+            if (bytes_read == bytes_sent && bytes_sent < BUF_SIZE) {
+                goto close_client;
+            }
+        }
+
+close_client:
+        close(new_fd);
+    }
+
+    // 0 = let sends finish, receives are disallowed
+    shutdown(sockfd, 0);
+
     return 0;
 }

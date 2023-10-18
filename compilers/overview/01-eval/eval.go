66c637f84b8d17b75e17fbe697fb3423c419dc4e
Eva Pace <eba.pachi@gmail.com>
2023-10-18 00:27:44 -0300

    compilers/overview: basic literal handling (one int)

--- compilers/overview/01-eval/eval.go
+++ compilers/overview/01-eval/eval.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"log"
+    "go/ast"
+    "go/parser"
+    "go/token"
+    "log"
+    "strconv"
 )
+
+// - integer literals
+// - basic arithmetic (+ - * /)
+// - parenthesis (grouping)
 
 // Given an expression containing only int types, evaluate
 // the expression and return the result.
 func Evaluate(expr ast.Expr) (int, error) {
-	// TODO
+    if basicLit, ok := expr.(*ast.BasicLit); ok {
+        // assume it's an integer (basicLit.Kind == INT)
+        return strconv.Atoi(basicLit.Value)
+    }
+
 	return 0, nil
 }
 

3f969300d9dec005d5e499fc1184af66c29e3e9a
Eva Pace <eba.pachi@gmail.com>
2023-10-29 22:41:58 -0300

    compilers/scanner: add token module

--- compilers/scanner/scanner/scanner.go
+++ compilers/scanner/scanner/scanner.go
@@ -1,16 +1,17 @@
 package scanner
 
-type Token struct {
-}
+import t "github.com/evaporei/interpreter/token"
 
 type Scanner struct {
+    src string
+    tokens []t.Token
 }
 
 func New(src string) *Scanner {
-    return &Scanner {}
+    var tokens []t.Token
+    return &Scanner { src, tokens }
 }
 
-func (s *Scanner) ScanTokens() []Token {
-    var tokens []Token
-    return tokens
+func (s *Scanner) ScanTokens() []t.Token {
+    return s.tokens
 }

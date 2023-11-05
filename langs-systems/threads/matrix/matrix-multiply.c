b5c6551e0d5602a149013cc36e2974eb7e8f4044
Eva Pace <eba.pachi@gmail.com>
2023-11-05 14:20:11 -0300

    langs-systems/threads: make 1 thread in matrix multiply

--- langs-systems/threads/matrix/matrix-multiply.c
+++ langs-systems/threads/matrix/matrix-multiply.c
@@ -19,7 +19,38 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
   }
 }
 
+struct ParallelData {
+    double **c, **a, **b;
+    int a_rows, a_cols, b_cols;
+};
+
+void *parallel_cb(void *ptr) {
+    struct ParallelData *d = (struct ParallelData*) ptr;
+    
+    for (int i = 0; i < d->a_rows; i++) {
+        for (int j = 0; j < d->b_cols; j++) {
+            d->c[i][j] = 0;
+            for (int k = 0; k < d->a_cols; k++)
+                d->c[i][j] += d->a[i][k] * d->b[k][j];
+        }
+    }
+}
+
 void parallel_matrix_multiply(double **c, double **a, double **b, int a_rows,
                           int a_cols, int b_cols) {
-	matrix_multiply(c, a, b, a_rows, a_cols, b_cols);
+    pthread_t thread1;
+    int iret1;
+
+    struct ParallelData data;
+
+    data.c = c;
+    data.a = a;
+    data.b = b;
+
+    data.a_rows = a_rows;
+    data.a_cols = a_cols;
+    data.b_cols = b_cols;
+
+    iret1 = pthread_create(&thread1, NULL, parallel_cb, (void*) &data);
+    pthread_join(thread1, NULL);
 }

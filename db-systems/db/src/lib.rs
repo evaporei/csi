c34c24aaf57e5d554de684f16436e08a1c5a8757
Eva Pace <eba.pachi@gmail.com>
2024-03-02 11:56:33 -0300

    db-systems: explain btree map module

--- db-systems/db/src/lib.rs
+++ db-systems/db/src/lib.rs
@@ -1,3 +1,6 @@
+// this is a copy/adaptation of one of the iterations of the
+// BTreeMap from the Rust standard library
+// commit in the compiler: b6edc59413f79016a1063c2ec6bc05516bc99cb6
 pub mod btree;
 // still doesn't abstract all fs operations
 // they are scattered across the code base

2deb37396d0db484f17045f1d4a70cefefd255b0
Eva Pace <eba.pachi@gmail.com>
2024-03-02 10:21:15 -0300

    db-systems: identify join (oh fuck how do I deal w/ it)

--- db-systems/db/src/main.rs
+++ db-systems/db/src/main.rs
@@ -375,7 +375,8 @@ impl Index {
 }
 
 // const QUERY: &str = "queries/simple.json";
-const QUERY: &str = "queries/multi-table.json";
+// const QUERY: &str = "queries/multi-table.json";
+const QUERY: &str = "queries/join.json";
 
 fn main() {
     let query = fs::read_to_string(QUERY).unwrap();
@@ -387,30 +388,46 @@ fn main() {
         .expect("there should be at least one table in the scan list");
     let scanners: Vec<FileScan> = scan.iter().map(|table| FileScan::new(&table)).collect();
 
-    for mut scanner in scanners {
-        let schema = Schema::new(scanner.table());
+    // TODO: ewww, make this pretty plsss
+    // this means we're doing table.field in the query, aka JOINs!!!
+    let should_join = query
+        .selection
+        .as_ref()
+        .map(|some| some.iter().any(|f| f.contains(".")))
+        .unwrap_or(false)
+        || query
+            .projection
+            .as_ref()
+            .map(|some| some.iter().any(|f| f.contains(".")))
+            .unwrap_or(false);
+    if should_join {
+        println!("woo join");
+    } else {
+        for mut scanner in scanners {
+            let schema = Schema::new(scanner.table());
 
-        let results: Vec<Row> = match (query.selection.clone(), query.projection.clone()) {
-            (Some(selection), Some(projection)) => {
-                let mut selector = Selector::new(selection, &mut scanner, &schema)
-                    .into_iter()
-                    .flatten();
-                let projector = Projector::new(projection, &mut selector, &schema);
-                projector.into_iter().collect()
-            }
-            (Some(selection), None) => {
-                let selector = Selector::new(selection, &mut scanner, &schema);
-                selector.into_iter().flatten().collect()
-            }
-            (None, Some(projection)) => {
-                let projector = Projector::new(projection, &mut scanner, &schema);
-                projector.into_iter().collect()
-            }
-            (None, None) => scanner.into_iter().collect(),
-        };
+            let results: Vec<Row> = match (query.selection.clone(), query.projection.clone()) {
+                (Some(selection), Some(projection)) => {
+                    let mut selector = Selector::new(selection, &mut scanner, &schema)
+                        .into_iter()
+                        .flatten();
+                    let projector = Projector::new(projection, &mut selector, &schema);
+                    projector.into_iter().collect()
+                }
+                (Some(selection), None) => {
+                    let selector = Selector::new(selection, &mut scanner, &schema);
+                    selector.into_iter().flatten().collect()
+                }
+                (None, Some(projection)) => {
+                    let projector = Projector::new(projection, &mut scanner, &schema);
+                    projector.into_iter().collect()
+                }
+                (None, None) => scanner.into_iter().collect(),
+            };
 
-        println!("results:");
-        println!("{results:?}");
+            println!("results:");
+            println!("{results:?}");
+        }
     }
 
     // index example/tests

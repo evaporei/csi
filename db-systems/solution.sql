c5b4ec2d1d32418090d156de48d996c892b8b8f7
Eva Pace <eba.pachi@gmail.com>
2024-03-06 11:44:05 -0300

    db-systems: solve third query

--- db-systems/solution.sql
+++ db-systems/solution.sql
@@ -102,3 +102,86 @@ limit 10;
 -- | 37756  | 150649             |
 -- | 43373  | 148990             |
 -- +--------+--------------------+
+
+-- 3. What is the total bonus amount awarded by department?
+select
+    e.dep_id as department,
+    sum(b.amount) as total_bonus
+from
+    bonus b
+join
+    employee e
+ON
+    e.emp_id = b.emp_id
+group by
+    e.dep_id;
+
+-- results
+-- +------------+-------------+
+-- | department | total_bonus |
+-- |------------+-------------|
+-- | 1          | 600876770   |
+-- | 2          | 199996518   |
+-- | 3          | 201135900   |
+-- +------------+-------------+
+
+-- explain
+-- +------------------------------------------------------------------------------------------------------+
+-- | QUERY PLAN                                                                                           |
+-- |------------------------------------------------------------------------------------------------------|
+-- | Finalize GroupAggregate  (cost=5988.61..5989.00 rows=3 width=12)                                     |
+-- |   Group Key: e.dep_id                                                                                |
+-- |   ->  Gather Merge  (cost=5988.61..5988.96 rows=3 width=12)                                          |
+-- |         Workers Planned: 1                                                                           |
+-- |         ->  Sort  (cost=4988.60..4988.61 rows=3 width=12)                                            |
+-- |               Sort Key: e.dep_id                                                                     |
+-- |               ->  Partial HashAggregate  (cost=4988.55..4988.58 rows=3 width=12)                     |
+-- |                     Group Key: e.dep_id                                                              |
+-- |                     ->  Hash Join  (cost=1641.00..4400.32 rows=117647 width=8)                       |
+-- |                           Hash Cond: (b.emp_id = e.emp_id)                                           |
+-- |                           ->  Parallel Seq Scan on bonus b  (cost=0.00..2450.47 rows=117647 width=8) |
+-- |                           ->  Hash  (cost=1016.00..1016.00 rows=50000 width=8)                       |
+-- |                                 ->  Seq Scan on employee e  (cost=0.00..1016.00 rows=50000 width=8)  |
+-- +------------------------------------------------------------------------------------------------------+
+
+-- now let's say we want the department names hehe :)
+select
+    e.dep_id as department,
+    d.name,
+    sum(b.amount) as total_bonus
+from
+    bonus b
+join employee e ON e.emp_id = b.emp_id
+join department d ON e.dep_id = d.dep_id
+group by
+    e.dep_id, d.name;
+
+-- +------------+-------------+-------------+
+-- | department | name        | total_bonus |
+-- |------------+-------------+-------------|
+-- | 1          | sales       | 600876770   |
+-- | 2          | marketing   | 199996518   |
+-- | 3          | engineering | 201135900   |
+-- +------------+-------------+-------------+
+
+-- +------------------------------------------------------------------------------------------------------------+
+-- | QUERY PLAN                                                                                                 |
+-- |------------------------------------------------------------------------------------------------------------|
+-- | Finalize GroupAggregate  (cost=7029.08..7030.27 rows=9 width=21)                                           |
+-- |   Group Key: e.dep_id, d.name                                                                              |
+-- |   ->  Gather Merge  (cost=7029.08..7030.12 rows=9 width=21)                                                |
+-- |         Workers Planned: 1                                                                                 |
+-- |         ->  Sort  (cost=6029.07..6029.09 rows=9 width=21)                                                  |
+-- |               Sort Key: e.dep_id, d.name                                                                   |
+-- |               ->  Partial HashAggregate  (cost=6028.84..6028.93 rows=9 width=21)                           |
+-- |                     Group Key: e.dep_id, d.name                                                            |
+-- |                     ->  Hash Join  (cost=1642.07..5146.49 rows=117647 width=17)                            |
+-- |                           Hash Cond: (e.dep_id = d.dep_id)                                                 |
+-- |                           ->  Hash Join  (cost=1641.00..4400.32 rows=117647 width=8)                       |
+-- |                                 Hash Cond: (b.emp_id = e.emp_id)                                           |
+-- |                                 ->  Parallel Seq Scan on bonus b  (cost=0.00..2450.47 rows=117647 width=8) |
+-- |                                 ->  Hash  (cost=1016.00..1016.00 rows=50000 width=8)                       |
+-- |                                       ->  Seq Scan on employee e  (cost=0.00..1016.00 rows=50000 width=8)  |
+-- |                           ->  Hash  (cost=1.03..1.03 rows=3 width=13)                                      |
+-- |                                 ->  Seq Scan on department d  (cost=0.00..1.03 rows=3 width=13)            |
+-- +------------------------------------------------------------------------------------------------------------+
